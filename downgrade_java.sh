7bfe5335d9a66f2857ae2ad660990f26eaf38c8e
hackapi <82610533+hackapi@users.noreply.github.com>
2022-09-15 15:01:11 +0200

    Create downgrade_java.sh

---
+++ downgrade_java.sh
@@ -0,0 +1,4 @@
+#! /bin/sh.
+cd /usr/lib/jvm
+sudo wget https://javadl.oracle.com/webapps/download/GetFile/1.8.0_321-b07/df5ad55fdd604472a86a45a217032c7d/linux-i586/jdk-8u321-linux-x64.tar.gz
+sudo tar -xvzf jdk-8u321-linux-x64.tar.gz
